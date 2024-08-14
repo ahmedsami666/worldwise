55da9cc973abcb844f45b3f3cb956509ecccd4e8
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-08-14 18:32:52 +0300

    setting app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-const App =() => {
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import Product from "./pages/Product"
+import Homepage from './pages/Homepage'
+import Pricing from './pages/Pricing'
+import Login from "./pages/Login"
+import AppLayout from "./pages/Applayout"
+import PageNotFound from "./pages/PageNotFound"
+
+const App = () => {
     return (
-        <div></div>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Homepage />}/>
+                <Route path="/product" element={<Product />}/>
+                <Route path="/pricing" element={<Pricing />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/app" element={<AppLayout />}>
+                    <Route index element={<p>List of cities</p>}/>
+                    <Route path="cities" element={<p>list of cities</p>}/>
+                    <Route path="countries" element={<p>list of countries</p>}/>
+                    <Route path="form" element={<p>Form</p>}/>
+                </Route>
+                <Route path="*" element={<PageNotFound />}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 export default App
---
+++ src/Product.jsx
@@ -0,0 +1,6 @@
+const Product = () => {
+    return (
+        <h1>product</h1>
+    )
+}
+export default Product
