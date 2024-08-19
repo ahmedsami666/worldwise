a14ddcc914f9e305ef333d5d11da7f638c0459cc
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-08-19 15:33:24 +0300

    setting map position to current location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,19 +3,29 @@ import styles from './Map.module.css'
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet'
 import { useEffect, useState } from 'react'
 import { useCities } from '../context/CitiesContext'
+import { useGeolocation } from '../hooks/useGeolocation'
+import Button from './Button'
 
 const Map = () => {
     const [searchParams, setSearchParams] = useSearchParams()
-    const mapLat = searchParams.get('lat')
-    const mapLng = searchParams.get('lng')
     const [mapPosition, setMapPosition] = useState([40, 0])
     const {cities} = useCities()
+    const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation()
+
+    const mapLat = searchParams.get('lat')
+    const mapLng = searchParams.get('lng')
     
     useEffect(() => {
         if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
     }, [mapLat, mapLng])
+    useEffect(() => {
+        if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    }, [geolocationPosition])
     return (
         <div className={styles.mapContainer}>
+           {!geolocationPosition && <Button type='position' onClick={getPosition}>
+                {isLoadingPosition ? 'loading...' : 'use your position'}
+            </Button>}
             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
